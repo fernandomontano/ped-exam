3cff0731c98f7eff6a375c9e563d93183f1dac0e
Ivania <ivanialebron4.180@gmail,com>
2022-04-14 00:23:29 -0600

    Genera el arbol AVL + recorridos

---
+++ PED_Exam/Ejercicio 2/AVL.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ejercicio_2
+{
+    class AVL
+    {
+        public int valor;
+        public AVL nodoizquierdo;
+        public AVL nododerecho;
+        public AVL nodopadre;
+        public int altura;
+        public AVL()
+        {
+
+        }
+        AVL raiz = null;
+        public void Insertar1(int dato)
+        {
+
+            if (raiz == null)
+            {
+                raiz = new AVL(dato, null, null, null);
+            }
+            else
+            {
+                raiz = raiz.Insertar(dato, raiz);
+            }
+        }
+        public void preordenrecursivo()
+        {
+            Console.WriteLine("Pre-orden");
+            preorden(raiz);
+        }
+        public void preorden(AVL raiz)
+        {
+            if (raiz != null)
+            {
+                Console.Write(raiz.valor.ToString() +" - ");
+                preorden(raiz.nodoizquierdo);
+                preorden(raiz.nododerecho);
+            }
+        }
+        public void inordenrecursivo()
+        {
+            Console.WriteLine("In-orden");
+            inorden(raiz);
+        }
+        public void inorden(AVL raiz)
+        {
+            if (raiz != null)
+            {
+                inorden(raiz.nodoizquierdo);
+                Console.Write(raiz.valor.ToString() + " - ");
+                inorden(raiz.nododerecho);
+            }
+        }
+        public void postordenrecursivo()
+        {
+            Console.WriteLine("Post-orden");
+            postorden(raiz);
+        }
+        public void postorden(AVL raiz)
+        {
+            if (raiz != null)
+            {
+                postorden(raiz.nodoizquierdo);
+                postorden(raiz.nododerecho);
+                Console.Write(raiz.valor.ToString() + " - ");
+            }
+        }
+
+        public AVL (int valornuevo, AVL izquierdo, AVL derecho, AVL padre)
+        {
+            valor = valornuevo;
+            nodoizquierdo = izquierdo;
+            nododerecho = derecho;
+            nodopadre = padre;
+            altura = 0;
+        }
+        public AVL Insertar(int pvalornuevo, AVL praiz)
+        {
+            if (praiz == null)
+            {
+                praiz = new AVL(pvalornuevo, null, null, null);
+            }
+            else if (pvalornuevo < praiz.valor)
+            {
+                praiz.nodoizquierdo = Insertar(pvalornuevo, praiz.nodoizquierdo);
+            }
+            else if (pvalornuevo > praiz.valor)
+            {
+                praiz.nododerecho = Insertar(pvalornuevo, praiz.nododerecho);
+            }
+            else
+            {
+                Console.WriteLine("Ya existe el valor");
+            }
+            if (alturas(praiz.nodoizquierdo) - alturas(praiz.nododerecho) == 2)
+            {
+                if (pvalornuevo < praiz.nodoizquierdo.valor) praiz = rotacionizquierdasimple(praiz);
+                else praiz = rotacionizquierdadoble(praiz);
+            }
+            if (alturas(praiz.nododerecho) - alturas(praiz.nodoizquierdo) == 2)
+            {
+                if (pvalornuevo > praiz.nododerecho.valor) praiz = rotacionderechasimple(praiz);
+                else praiz = rotacionderechadoble(praiz);
+            }
+            praiz.altura = max(alturas(praiz.nodoizquierdo), alturas(praiz.nododerecho)) + 1;
+            return praiz;
+        }
+
+        private static int max(int lhs, int rhs)
+        {
+            return lhs > rhs ? lhs : rhs;
+        }
+        private static int alturas (AVL raiz)
+        {
+            return raiz == null ? -1 : raiz.altura;
+        }
+ 
+        private static AVL rotacionizquierdasimple(AVL k2)
+        {
+            AVL k1 = k2.nodoizquierdo;
+            k2.nodoizquierdo = k1.nododerecho;
+            k1.nododerecho = k2;
+            k2.altura = max(alturas(k2.nodoizquierdo), alturas(k2.nododerecho)) + 1;
+            k1.altura = max(alturas(k1.nodoizquierdo), k2.altura) + 1;
+            return k1;
+        }
+        private static AVL rotacionderechasimple(AVL k1)
+        {
+            AVL k2 = k1.nododerecho;
+            k1.nododerecho = k2.nodoizquierdo;
+            k2.nodoizquierdo = k1;
+            k1.altura = max(alturas(k1.nodoizquierdo), alturas(k1.nododerecho)) + 1;
+            k2.altura = max(alturas(k2.nododerecho), k1.altura) + 1;
+            return k2;
+        }
+        private static AVL rotacionizquierdadoble(AVL k3)
+        {
+            k3.nodoizquierdo = rotacionderechasimple(k3.nodoizquierdo);
+            return rotacionizquierdasimple(k3);
+        }
+        private static AVL rotacionderechadoble(AVL k4)
+        {
+            k4.nododerecho = rotacionizquierdasimple(k4.nododerecho);
+            return rotacionderechasimple(k4);
+        }
+        public int getaltura(AVL nodoactual)
+        {
+            if (nodoactual == null)
+            {
+                return 0;
+            }
+            else
+            {
+                return 1 + Math.Max(getaltura(nodoactual.nodoizquierdo), getaltura(nodoactual.nododerecho));
+            }
+        }
+        public void buscar(int valorbuscar, AVL raiz)
+        {
+            if (raiz != null)
+            {
+                if (valorbuscar < raiz.valor)
+                {
+                    buscar(valorbuscar, raiz.nodoizquierdo);
+                }
+                else
+                {
+                    if (valorbuscar > raiz.valor)
+                    {
+                        buscar(valorbuscar, raiz.nododerecho);
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("Valor no encontrado");
+            }  
+        }
+    }
+}
--- PED_Exam/Ejercicio 2/Program.cs
+++ PED_Exam/Ejercicio 2/Program.cs
@@ -4,9 +4,27 @@ namespace Ejercicio_2
 {
     class Program
     {
+        public static AVL raiz = null;
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            AVL arbolito = new AVL();
+
+           int[] valoresarbol = new int[] { 50,17,9,14,23,54,76,12,19,72,67 };
+            for (int i = 0; i < valoresarbol.Length; i++)
+            {
+                arbolito.Insertar1(valoresarbol[i]);
+            }
+            arbolito.dibujararbol();
+            arbolito.preordenrecursivo();
+            Console.WriteLine();
+            arbolito.inordenrecursivo();
+            Console.WriteLine();
+            arbolito.postordenrecursivo();
+            Console.ReadKey();
+        }
+        public static void dibujaravl()
+        {
+
         }
     }
 }
